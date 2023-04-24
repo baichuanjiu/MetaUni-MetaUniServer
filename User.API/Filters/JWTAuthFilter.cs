bd8b911033f67deefea2668e6ef91bf752d57608
baichuanjiu <baichuanjiu@gmail.com>
2023-04-24 20:02:18 +0800

    为JWTAuthFilter增加了日志支持

--- User.API/Filters/JWTAuthFilter.cs
+++ User.API/Filters/JWTAuthFilter.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text.Json;
+using User.API.Controllers.Profile;
 using User.API.ReusableClass;
 
 namespace User.API.Filters
@@ -10,10 +11,12 @@ namespace User.API.Filters
     {
         //依赖注入
         private readonly IDistributedCache _distributedCache;
+        private readonly ILogger<JWTAuthFilterService> _logger;
 
-        public JWTAuthFilterService(IDistributedCache distributedCache)
+        public JWTAuthFilterService(IDistributedCache distributedCache, ILogger<JWTAuthFilterService> logger)
         {
             _distributedCache = distributedCache;
+            _logger = logger;
         }
 
         async Task IAsyncActionFilter.OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
@@ -22,6 +25,7 @@ namespace User.API.Filters
             string? currentJWT = await _distributedCache.GetStringAsync(context.ActionArguments["UUID"]!.ToString()!);
             if (currentJWT != context.ActionArguments["JWT"] as string)
             {
+                _logger.LogWarning("Warning：用户[ {UUID} ]在访问[ {controller} ]时使用了无效的JWT", context.ActionArguments["UUID"]!.ToString()!, context.Controller.ToString());
                 ResponseT<string> authorizationFailed = new(1, "使用了无效的JWT，请重新登录");
                 context.Result = new ContentResult
                 {
