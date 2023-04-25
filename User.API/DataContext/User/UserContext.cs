30f64a6246e7133dd84c39effc105192c213ae8d
baichuanjiu <baichuanjiu@gmail.com>
2023-04-25 20:34:12 +0800

    feat(User.API): 新增Friend相关的实体类，Group相关的实体类（Group设计后续还会再更改）

--- User.API/DataContext/User/UserContext.cs
+++ User.API/DataContext/User/UserContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using User.API.Entities.Friend;
 using User.API.Entities.User;
 
 namespace User.API.DataContext.User
@@ -12,6 +13,9 @@ namespace User.API.DataContext.User
         public DbSet<UserAccount> UserAccounts { get; set; }
         public DbSet<UserProfile> UserProfiles { get; set; }
         public DbSet<UserSyncTable> UserSynchronizationTables { get; set; }
+        public DbSet<FriendsGroup> FriendsGroups { get; set; }
+        public DbSet<FriendShip> FriendShips { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -20,6 +24,8 @@ namespace User.API.DataContext.User
             modelBuilder.Entity<UserAccount>().Property(u => u.UUID).HasDefaultValueSql("NEXT VALUE FOR CurrentUUID");
             modelBuilder.Entity<UserProfile>();
             modelBuilder.Entity<UserSyncTable>();
+            modelBuilder.Entity<FriendsGroup>();
+            modelBuilder.Entity<FriendShip>();
         }
 
     }
---
+++ User.API/Entities/Group/GroupShip.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace User.API.Entities.Group
+{
+    [Table("GroupShip")]
+    [Index(nameof(GroupId))]
+    [Index(nameof(UUID))]
+    public class GroupShip
+    {
+        [Key]
+        public int Id { get; set; } //主键
+        public int GroupId { get; set; } //逻辑外键，与Group表关联，群号，群组的唯一标识
+        public int UUID { get; set; } //逻辑外键，与UserAccount表关联，标识这条GroupShip属于谁，主体的UUID
+        public DateTime GroupJoinTime { get; set; } //主体的入群时间
+        [MaxLength(13)]
+        public string Role { get; set; } //标识主体在该群组内的角色，Leader、Administrator、Member
+        public bool DoNotDisturbMode { get; set; } = false;  //主体是否对该群组开启免打扰模式
+        [MaxLength(10)]
+        public string? NicknameInGroup { get; set; } //主体在该群组内的群昵称
+        public int SequenceWhenJoined { get; set; } //主体在加入该群组时，群组内的最新一条消息的Sequence，用于消息对齐
+        public int Sequence { get; set; } //主体在该群组内获取到的最新一条消息的Sequence，用于消息对齐
+
+    }
+}
