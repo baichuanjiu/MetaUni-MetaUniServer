9d571ac9a9354226c95e6f450cfa1c1440bee843
baichuanjiu <baichuanjiu@gmail.com>
2023-05-14 19:49:27 +0800

    refactor(ReadMessagesRequestData): 修改了数据结构

--- WebSocket.API/ReusableClass/ReadMessagesRequestData.cs
+++ WebSocket.API/ReusableClass/ReadMessagesRequestData.cs
@@ -2,15 +2,11 @@
 {
     public class ReadMessagesRequestData
     {
-        public ReadMessagesRequestData(int UUID, string JWT, int chatId)
+        public ReadMessagesRequestData(int chatId)
         {
-            this.UUID = UUID;
-            this.JWT = JWT;
             ChatId = chatId;
         }
 
-        public int UUID { get; set; }
-        public string JWT { get; set; }
         public int ChatId { get; set; }
     }
 }
